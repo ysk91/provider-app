78cda42a54510e2767da494bece39f6606f79655
ysk91 <ysk91.engineer@gmail.com>
2023-03-23 12:43:59 +0900

    Userモデルを作成

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name,              presence: true,
+                                length: { maximum: 50 }
+                                # uniqueness: { scope: :group_id }
+                                # format: false
+
+  validates :email,             # presence: true,
+                                # length: { maximum: 50 }
+                                uniqueness: true
+                                # format: false
+
+  validates :uid,               # presence: true
+                                # length: { maximum: 50 }
+                                uniqueness: true
+                                # format: false
+
+  validates :provider,          presence: true
+                                # length: { maximum: 50 }
+                                # uniqueness: true
+                                # format: false
+
+  after_create :set_uid!
+
+  private
+
+  def set_uid!
+    update(uid: SecureRandom.uuid)
+  end
+
+end
