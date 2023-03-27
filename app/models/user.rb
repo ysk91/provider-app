e7a8a12b46bcd137a00ca9c779ce84d491371951
ysk91 <ysk91.engineer@gmail.com>
2023-03-27 15:17:29 +0900

    annotate gemを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           default(""), not null
+#  encrypted_password  :string           default(""), not null
+#  name                :string           default(""), not null
+#  remember_created_at :datetime
+#  uid                 :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           default(""), not null
+#  encrypted_password  :string           default(""), not null
+#  name                :string           default(""), not null
+#  remember_created_at :datetime
+#  uid                 :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
